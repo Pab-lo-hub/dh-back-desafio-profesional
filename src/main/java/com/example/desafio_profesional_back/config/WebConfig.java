c5f5d67348abcd6f4b933e8ef3afee99f321f401
Pablo S <salaberry.pablo@gmail.com>
2025-04-06 14:01:58 -0300

    Nueva clase de WebConfig para aceptar peticiones del front por CORS

---
+++ src/main/java/com/example/desafio_profesional_back/config/WebConfig.java
@@ -0,0 +1,18 @@
+// src/main/java/com/example/desafio_profesional_back/config/WebConfig.java
+package com.example.desafio_profesional_back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("http://localhost:3000") // Permitir el origen del frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Todos los encabezados
+                .allowCredentials(false); // Si usas cookies o autenticación
+    }
+}
